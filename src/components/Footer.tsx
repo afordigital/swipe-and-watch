3dc79b862ffa3b3d4095612e1446196265fae73e
Marcos S <marcosgnasa@gmail.com>
2024-10-10 23:18:38 +0200

    fix: responsive card height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex-grow flex h-full items-end text-[#64748B] text-[14px]">
+    <footer className="text-[#64748B] text-[14px]">
       Designed and developed by aforcita
     </footer>
   );
